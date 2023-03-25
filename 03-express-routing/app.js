f053bd580f458f2b5d7f17c3aa100ebe0bfe9d92
mfirmanakbar <em.firman.akbar@gmail.com>
2023-03-25 12:19:22 +0700

    express serving static files

--- 03-express-routing/app.js
+++ 03-express-routing/app.js
@@ -18,6 +18,8 @@ app.delete('/user', (req, res) => {
     res.send('Got a DELETE request at /user')
 })
 
+app.use(express.static('public'))
+
 app.listen(port, () => {
     console.log(`App runnning on port ${port}`);
 })
